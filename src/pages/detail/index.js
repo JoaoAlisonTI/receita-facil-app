0e2fa174e75cc39072ea170870a080bca5ad629c
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-06-19 13:34:29 -0300

    Add json api ingredients and details

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,8 +1,8 @@
 import { useLayoutEffect } from 'react'
-import { View, Text, StyleSheet, Pressable } from 'react-native'
+import { View, Text, StyleSheet, Pressable, ScrollView, Image } from 'react-native'
 import { useRoute, useNavigation } from '@react-navigation/native'
 
-import { Entypo } from '@expo/vector-icons'
+import { Entypo, AntDesign, Feather } from '@expo/vector-icons'
 
 export function Detail() {
   const route = useRoute();
@@ -26,14 +26,68 @@ export function Detail() {
   }, [navigation, route.params?.data])
   
   return (
-      <View style={styles.container}>
-        <Text>{route.params?.data.name}</Text>
-      </View>
+      <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+        <Pressable>
+          <View style={styles.playIcon}>
+            <AntDesign name="playcircleo" size={48} color="#fafafa" />
+          </View>
+          <Image 
+            source={{ uri: route.params?.data.cover }}
+            style={styles.cover}
+          />
+        </Pressable>
+        
+        <View style={styles.headerDetails}>
+          <View>
+            <Text style={styles.title}>
+              {route.params?.data.name}
+            </Text>
+            <Text style={styles.ingredientsText}>
+              Ingredientes ({route.params?.data.total_ingredients})
+            </Text>
+          </View>
+          <Pressable>
+              <Feather name="share-2" size={24} color="#121212" />
+            </Pressable>
+        </View>
+        
+      </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
   container:{
-    backgroundColor: 'white',
+    backgroundColor: '#f3f9ff',
+    paddingTop: 14,
+    paddingEnd: 14,
+    paddingStart: 14
+  },
+  cover:{
+    height: 200,
+    borderRadius: 14,
+    width: '100%'
+  },
+  playIcon:{
+    position: 'absolute',
+    zIndex: 9,
+    top: 0, left: 0, right: 0, bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title:{
+    fontSize: 18,
+    marginTop: 14,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 4
+  },
+  ingredientsText:{
+    marginBottom: 14,
+    fontSize: 16
+  },
+  headerDetails:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   }
 })
